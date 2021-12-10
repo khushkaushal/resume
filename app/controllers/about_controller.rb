a074cc1ae6b9405614bf59af5d244e21c6c8bd27
khushkaushal <khushkaushal62@yahoo.com>
2021-12-09 16:52:55 -0800

    changes to resume

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -4,5 +4,10 @@ class AboutController < ApplicationController
 
   def contact
   end
+
+  def pdf
+    pdf_filename = File.join(Rails.root, "tmp/Resume.pdf")
+    send_file(pdf_filename, :filename => "Resume.pdf", :disposition => 'inline', :type => "application/pdf")
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get "/analytics", to: "projects#analytics"
   get "/mdss", to: "projects#mdss"
   get "/mechanic", to: "projects#mechanic"
+  get "/resume", to: "about#pdf"
 end
